ede56d8a9c306373a67da78b9bce34ab08175fc6
Shubhang Tiwari <shubh.bitsmith@gmail.com>
2022-01-22 02:32:14 +0530

    HLS Recording Started

--- utils/Types.ts
+++ utils/Types.ts
@@ -1,8 +1,4 @@
 import { Logger } from './Logger';
-import { CONFIG } from '../constants/configuration';
-import { schedule } from 'node-cron';
-import { mkdirSync } from 'fs';
-import { appendZero } from './Utility';
 
 export abstract class Service<ServiceType> {
   logger: Logger;
@@ -10,29 +6,9 @@ export abstract class Service<ServiceType> {
   camera: Camera;
   path: string | undefined;
 
-  constructor(camera: Camera, component: string, path?: string, enableCron?: boolean) {
+  constructor(camera: Camera, component: string) {
     this.camera = camera;
     this.logger = new Logger(component);
-    this.path = path;
-
-    if (path) {
-      this.initializePath(true);
-
-      if (enableCron) {
-        this.logger.debug('Enabled automatic directory creation daily at 11.59 PM');
-        schedule('59 23 * * *', this.initializePath.bind(this, false));
-      }
-    }
-  }
-
-  initializePath(today: boolean = false) {
-    let now = new Date();
-    let date = appendZero(today ? now.getDate() : now.getDate() + 1);
-    let month = appendZero(now.getMonth() + 1);
-    let year = now.getFullYear();
-    
-    this.logger.debug(`Creating ${CONFIG.ROOT_PATH}/${this.path}/${year}${month}${date} for recording`);
-    mkdirSync(`${CONFIG.ROOT_PATH}/${this.path}/${year}${month}${date}`, { recursive: true });
   }
 
   abstract start(): Promise<ServiceType>;
